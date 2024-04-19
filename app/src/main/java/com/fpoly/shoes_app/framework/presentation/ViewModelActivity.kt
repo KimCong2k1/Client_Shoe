e75f96740970cba0cd0e065f5aee9f5357fd244b
congpgph26764 <117719368+congpgph26764@users.noreply.github.com>
2024-04-19 20:43:17 +0700

    Revert "add base code"

--- app/src/main/java/com/fpoly/shoes_app/framework/presentation/ViewModelActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.fpoly.shoes_app.framework.presentation
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-
-class ViewModelActivity: ViewModel() {
-}
--- app/src/main/java/com/fpoly/shoes_app/utility/Constans.kt
+++
@@ -1,2 +0,0 @@
-package com.fpoly.shoes_app.utility
-

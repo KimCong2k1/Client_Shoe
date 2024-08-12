a504e15742526a56c3234ecb6aca56fc8fc9a547
ThangDev17B9 <nq.thangdev@gmail.com>
2024-08-12 15:10:20 +0700

    update ui icon part 6

---
+++ app/src/main/java/com/fpoly/shoes_app/framework/presentation/ui/paymentMethod/PaymentMethodsFragment.kt
@@ -0,0 +1,31 @@
+package com.fpoly.shoes_app.framework.presentation.ui.paymentMethod
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.fpoly.shoes_app.R
+
+class PaymentMethodsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = PaymentMethodsFragment()
+    }
+
+    private val viewModel: PaymentMethodsViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_payment_methods, container, false)
+    }
+}

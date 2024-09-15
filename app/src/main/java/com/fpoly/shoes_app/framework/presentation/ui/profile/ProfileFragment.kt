f0603b621991d18a307ca6b7cac8158364d296aa
quylhph28754 <quylhph28754@fpt.edu.vn>
2024-09-15 23:29:48 +0700

    Update Login Address In Profile

--- app/src/main/java/com/fpoly/shoes_app/framework/presentation/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/fpoly/shoes_app/framework/presentation/ui/profile/ProfileFragment.kt
@@ -47,7 +47,6 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding, SetUpAccountViewMod
     private lateinit var pickImageLauncher: ActivityResultLauncher<Intent>
     private lateinit var idUser: String
     private var bmp: Bitmap? = null
-
     private val navOptions =
         NavOptions.Builder().setEnterAnim(R.anim.slide_in_right).setExitAnim(R.anim.slide_out_left)
             .setPopEnterAnim(R.anim.slide_in_left).setPopExitAnim(R.anim.slide_out_right).build()

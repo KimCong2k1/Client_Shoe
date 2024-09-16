06c9a3d6e376d905b33bb486209349ff78690343
ThangDev17B9 <nq.thangdev@gmail.com>
2024-09-16 20:57:10 +0700

    update UI

--- app/src/main/java/com/fpoly/shoes_app/utility/Constans.kt
+++ app/src/main/java/com/fpoly/shoes_app/utility/Constans.kt
@@ -3,6 +3,8 @@ package com.fpoly.shoes_app.utility
 
 class Constants {
     companion object {
+        //192.168.1.10
+        //192.168.88.124
         const val BASE_URL = "http://192.168.88.124:3000/api/"
         const val SET_TIME_OUT_API = 100L
     }
